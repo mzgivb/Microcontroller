ae2ece8cb2ca5825c6bef142072f68e57a04b544
mzgivb <jochen.leeder@mzgivb.de>
2025-03-04 13:28:41 +0100

    adc

--- poti_auslesen.py
+++ poti_auslesen.py
@@ -1,11 +1,12 @@
 from machine import ADC, Pin
 import time
 
-adc = ADC(Pin(34))  # Pin anpassen
-adc.width(ADC.WIDTH_10BIT)   # 10 Bit Auflösung
-adc.atten(ADC.ATTN_11DB)     # Messbereich erweitern
+adc = ADC(Pin(12))  # Pin anpassen
+  
+adc.atten(ADC.ATTN_11DB) # Messbereich erweitern
+adc.width(ADC.WIDTH_12BIT) # 12 Bit Auflösung
 
 while True:
-    wert = adc.read()
+    wert = (adc.read() /4)
     print("ADC-Wert:", wert)
     time.sleep(0.5)
